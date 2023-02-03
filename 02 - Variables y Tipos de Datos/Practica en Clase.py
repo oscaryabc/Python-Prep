4067e6e745da3735517ef51381dfc3a1e9681432
oscaryabc <oscaryabc77@gmail.com>
2023-02-02 22:45:19 -0500

    first commit

--- 02 - Variables y Tipos de Datos/Practica en Clase.py
+++ 02 - Variables y Tipos de Datos/Practica en Clase.py
@@ -1,6 +1,6 @@
 mi_variable = 12
 print(mi_variable)
-
+ 
 mi_variable2 = 'dario'
 print(mi_variable2)
 
@@ -10,3 +10,8 @@ print(mi_complejo)
 
 mi_numero = input('Ingrese un valor')
 print('el valor ingresado fue', mi_numero)
+
+tractores_del_dia  = (10 + 22)
+print (tractores_del_dia)
+
+del (mi_numero)
